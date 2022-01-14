9e3ad60293142179097daaaea00f98fc2299f7bc
Virti7702 <97676077+Virti7702@users.noreply.github.com>
2022-01-14 15:04:03 +0530

    Add files via upload

---
+++ pratical_2_2_e.py
@@ -0,0 +1,9 @@
+'''
+Name - Virti Shah
+ID - 20CE135
+Aim - A python program to find the length of tuple.
+Github link - https://github.com/Virti7702/Practical2-2-e.git
+
+'''
+t=(1,2,3,4)
+print(len(t))
